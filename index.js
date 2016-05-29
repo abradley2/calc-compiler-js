b207c01a3feccf3690c97e99af6eb8501688c43b
Tony <tony@localhost.localdomain>
2016-05-29 12:15:14 -0400

    wip, now takes a whole block of expressions

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ var Compiler = require('./lib/Compiler'),
     c = new Compiler()
 
 
-var testTemplate = '5 + ADD(COL_1 + 2 , 7) * 3'
+var testTemplate = 'COL_2 = 5 + ADD(COL_1 + 2 , 7) * 3'
 
-c.compileTemplate( testTemplate )
+var data = [
+    {COL_1: 2},
+    {COL_1: 7},
+    {COL_1: 1},
+    {COL_1: 43}
+]
+
+var outFunc = c.compileTemplate( testTemplate )
+
+data.map( outFunc, data )
 
 module.exports = Compiler

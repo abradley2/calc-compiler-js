dde28355f87ec0bbc8338d7bd7ea50636ee2d77b
Tony Bradley <antbradley91@gmail.com>
2016-06-06 23:12:30 -0400

    gh pages

--- dist/CALC.js
+++ dist/CALC.js
@@ -1,18 +1,24 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.CalcCompiler = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.CALC = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Compiler = require('./lib/Compiler'),
     c = new Compiler()
 
-//var testTemplate = 'SUM(1 - ZERO() - 8, SUM(3, 1, 20), 4)'
-var testTemplate = 'SUM(1, 2 * 3)'
+//var testTemplate = 'SUM(1 - 8, DIFF(3, 1, 20), 4 * price)'
+var testTemplate = 'SUM(A, 2 * B)'
 
 var data = [
-    {COL_1: 2},
-    {COL_1: 7},
-    {COL_1: 1},
-    {COL_1: 43}
+    {A: 2, B: 3},
+    {A: 7, B: 8},
+    {A: 1, B: 1},
+    {A: 43, B: 19}
 ]
 
 var outStack = c.parse( testTemplate )
+
+var outFunc = c.generate( outStack )
+
+var result = outFunc( data[0] )
+
+console.log('result = ', result)
 
 module.exports = Compiler
 
@@ -20,7 +26,7 @@ module.exports = Compiler
 var grammar = require('./grammar'),
     Tokenizer = require('./Tokenizer'),
     Tree = require('./Tree'),
-    func = require('./func'),
+    funcLib = require('./func'),
     assign = require('./util/assign'),
     omit = require('./util/omit'),
     getIndexBy = require('./util/getIndexBy'),
@@ -34,10 +40,6 @@ var funcMap = {
     '^': 'POW'
 }
 
-function cleanStackNode (node) {
-    return omit(node, 'funcDepth')
-}
-
 function Compiler () {
 
     this.tokenizer = new Tokenizer( grammar )
@@ -46,89 +48,73 @@ function Compiler () {
 
 assign(Compiler.prototype, {
 
-    /*
-        Small issue with RNP is it has trouble dealing with functions that
-        take a variable amount of arguments. So in the Shunting yard 
-        process in the Tree generator, a funcDepth attribute was added while
-        parsing the arguments to keep track of which args belonged to which 
-        function
-    */
-    _getFunctionArgs: function (funcNode, operands) {
-        var argIdx = getIndexBy(operands, {funcDepth: funcNode.funcDepth}),
-            args = []
-
-        if (argIdx !== -1) {
-            args = operands.splice(argIdx)
-        }
-
-        return args
-    },
-
     parse: function (template) {
 
         var tokens = this.tokenizer.getTokens(template),
             tree = new Tree(tokens),
             nodes = tree.parse()
 
-        var operands = [],
-            stack = []
-
-        console.log('NODES = ',nodes)
-
-        while (nodes.length > 0) {
-            
-            var node = nodes.shift(),
-                args = []
-
-            switch (node.type) {
-                case 'CONST':
-                    operands.push({
-                        type: 'CONST',
-                        funcDepth: node.funcDepth,
-                        content: node.content
-                    })
-                    break
-                case 'FUNC':
-                    stack.push({
-                        type: 'FUNC',
-                        content: node.content,
-                        args: this._getFunctionArgs(node, operands)//args.map(cleanStackNode)
-                    })
-                    break
-                case 'OPERATOR':
-                    /*
-                        Convert operators to appropriate functions
-                    */
-                    stack.push({
-                        type: 'FUNC',
-                        content: funcMap[node.content],
-                        args: operands.splice(0)//args.map(cleanStackNode)
-                    })
-                    break
-                default:
-                    break
+        /*
+            Simple cleanup to get everything in order
+        */
+        var stack = nodes.map(function (node) {
+            if (node.type === 'OPERATOR') {
+                node = {
+                    type: 'FUNC',
+                    name: funcMap[node.content],
+                    arity: 2
+                }
             }
-        }
+            return node
+        })
 
         return stack
 
     },
 
-    generate: function () {
-        /* 
-            Going to omit the actual compilation to a string. 
-            This is best used to create and use functions in 
-            memory. 
+    generate: function (_stack) {
+        // here to remind me to add a 'create closure' function
+        // for to-string compiling
+        var stack = _stack
 
-            Eval is evil.
-        */
+        return function (ctx) {
+            var operands = []
+
+            stack.forEach(function (node, idx) {
+
+                if (node.type === 'VAR' || node.type === 'CONST') {
+                    operands.push(node)
+                }
+                if (node.type === 'FUNC') {
+                    // execute the function, then store
+                    // its result as a const on the
+                    // operand stack
+
+                    var start = operands.length - node.arity,
+                        count = node.arity,
+                        args = operands.splice(start, count),
+                        func = funcLib[node.name]
+
+                    var result = func(args, ctx)
+
+                    operands.push({
+                        type: 'CONST',
+                        value: result
+                    })
+
+                }
+            })
+            // the last remaining CONST is the returned value
+            return last(operands).value
+        }
+
     }
 
 })
 
 module.exports = Compiler
 
-},{"./Tokenizer":3,"./Tree":4,"./func":12,"./grammar":13,"./util/assign":14,"./util/getIndexBy":16,"./util/last":18,"./util/omit":19}],3:[function(require,module,exports){
+},{"./Tokenizer":3,"./Tree":4,"./func":11,"./grammar":12,"./util/assign":13,"./util/getIndexBy":15,"./util/last":17,"./util/omit":18}],3:[function(require,module,exports){
 var assign = require('./util/assign')
 
 function Tokenizer (grammar) {
@@ -173,7 +159,7 @@ assign(Tokenizer.prototype, {
 
 module.exports = Tokenizer
 
-},{"./util/assign":14}],4:[function(require,module,exports){
+},{"./util/assign":13}],4:[function(require,module,exports){
 var assign = require('./util/assign'),
     last = require('./util/last')
 
@@ -183,13 +169,6 @@ function Tree (tokens) {
 
 assign(Tree.prototype, {
 
-    /*
-        Because of what we are compiling, we have no more than a single
-        block of expressions. Makes this task much easier than it would
-        be. For future reference should this be expanded to support
-        multiple blocks, this function should be renamed to 'parseBlock'
-        and called internally from the master 'parse' function
-    */
     parse: function () {
 
         // create a clone of the tokens array so it is not mutated
@@ -201,14 +180,26 @@ assign(Tree.prototype, {
         return this.getExpressionNode(tokens)
     },
 
-    /*
-        function for removing all whitespace token
-    */
+    // function for removing all whitespace tokens
     removeWhitespaceTokens: function (tokens) {
         tokens = tokens.filter(function (token) {
             return token.name !== 'WHITESPACE'
         })
         return tokens
+    },
+
+    getConst: function (token) {
+        if (token.found.match(/^[a-zA-Z]/)) {
+            return {
+                type: 'VAR',
+                name: token.found
+            }
+        } else {
+            return {
+                type: 'CONST',
+                value: token.found
+            }
+        }
     },
 
 
@@ -232,26 +223,18 @@ assign(Tree.prototype, {
                         arity.push(1)
                         stack.push({
                             type: 'FUNC',
-                            content: token.found
+                            name: token.found
                         })
                     } else {
-
-                        output.push({
-                            type: 'CONST',
-                            content: token.found
-                        })
-
+                        output.push(this.getConst(token))
                     } 
-
                     break
 
                 case 'BEG_ARGS':
-
                     stack.push({
                         type: 'BEG_ARGS',
                         content: token.found
                     })
-
                     break
 
                 case 'END_ARGS':
@@ -289,7 +272,6 @@ assign(Tree.prototype, {
                     if ( stack.length === 0 || last(stack).type !== 'BEG_ARGS' ) {
                         throw new Error('Missing openining  ( in arguments list')
                     }
-
                     break
 
                 case 'OPERATOR':
@@ -303,9 +285,8 @@ assign(Tree.prototype, {
                         associative = 'left'
                     }
 
-                    /*
-                        while there is an operator token o2, at the top of the operator stack and either
-                    */
+                    
+                    // while there is an operator token o2, at the top of the operator stack and either
                     while (last(stack) && last(stack).type === 'OPERATOR') {
                         // o1 is left-associative and its precedence is less than or equal to that of o2, or...
                         if (associative === 'left' && precedence <= last(stack).precedence ) {
@@ -342,14 +323,11 @@ assign(Tree.prototype, {
 
 module.exports = Tree
 
-},{"./util/assign":14,"./util/last":18}],5:[function(require,module,exports){
+},{"./util/assign":13,"./util/last":17}],5:[function(require,module,exports){
 var delimiters = {
     BEG_ARGS: '(',
     END_ARGS: ')',
-    END_EXPR: ';',
-    USE_VAR: '$',
-    ARG_SEP: ',',
-    ASSIGN: '='
+    ARG_SEP: ','
 }
 
 module.exports = delimiters
@@ -358,7 +336,22 @@ module.exports = delimiters
 var assign = require('../util/assign')
 
 function DIFF (args, ctx) {
+    var retVal
 
+    args.forEach(function (arg) {
+        var mod
+
+        if (arg.type === 'VAR') {
+            mod = parseFloat(ctx[arg.name])
+        } else {
+            mod = parseFloat(arg.value)
+        }
+
+        if (retVal) { retVal -= mod } else { retVal = mod }
+
+    })
+
+    return retVal
 }
 
 assign(DIFF.prototype, {
@@ -371,7 +364,7 @@ assign(DIFF.prototype, {
 
 module.exports = DIFF
 
-},{"../util/assign":14}],7:[function(require,module,exports){
+},{"../util/assign":13}],7:[function(require,module,exports){
 var assign = require('../util/assign')
 
 function POW (args, ctx) {
@@ -388,11 +381,21 @@ assign(POW.prototype, {
 
 module.exports = POW
 
-},{"../util/assign":14}],8:[function(require,module,exports){
+},{"../util/assign":13}],8:[function(require,module,exports){
 var assign = require('../util/assign')
 
 function PROD (args, ctx) {
+    var retVal = 1
 
+    args.forEach(function (arg) {
+        if (arg.type === 'VAR') {
+            retVal = retVal * parseFloat(ctx[arg.name])
+        } else {
+            retVal = retVal * parseFloat(arg.value)
+        }
+    })
+
+    return retVal
 }
 
 assign(PROD.prototype, {
@@ -405,7 +408,7 @@ assign(PROD.prototype, {
 
 module.exports = PROD
 
-},{"../util/assign":14}],9:[function(require,module,exports){
+},{"../util/assign":13}],9:[function(require,module,exports){
 var assign = require('../util/assign')
 
 function QUOT (args, ctx) {
@@ -422,39 +425,23 @@ assign(QUOT.prototype, {
 
 module.exports = QUOT
 
-},{"../util/assign":14}],10:[function(require,module,exports){
-var assign = require('../util/assign')
-
-function ROOT (ctx, funcs) {
-    this.ctx = ctx
-    this.funcs = funcs
-}
-
-assign(ROOT.prototype, {
-
-    compile: function () {
-        /*
-            TODO: finish string compilation
-            honestly, shouldn't really need this.
-            But nice extra feature.
-        */
-
-        // need to return the functions as a proper closure.
-        return (
-            ";(function(){" +
-                "var ctx = " + JSON.stringify(this.ctx) +
-            "});"
-        )
-    }
-
-})
-
-module.exports = ROOT
-},{"../util/assign":14}],11:[function(require,module,exports){
+},{"../util/assign":13}],10:[function(require,module,exports){
 var assign = require('../util/assign')
 
 function SUM (args, ctx) {
+    var retVal = 0
 
+    args.forEach(function (arg) {
+
+        if (arg.type === 'VAR') {
+            retVal += parseFloat(ctx[arg.name])
+        } else {
+            retVal += parseFloat(arg.value)
+        }
+
+    })
+
+    return retVal
 }
 
 assign(SUM.prototype, {
@@ -467,17 +454,16 @@ assign(SUM.prototype, {
 
 module.exports = SUM
 
-},{"../util/assign":14}],12:[function(require,module,exports){
+},{"../util/assign":13}],11:[function(require,module,exports){
 module.exports = {
     'SUM': require('./SUM'),
     'DIFF': require('./DIFF'),
     'QUOT': require('./QUOT'),
     'PROD': require('./PROD'),
-    'POW': require('./POW'),
-    'ROOT': require('./ROOT')
+    'POW': require('./POW')
 }
 
-},{"./DIFF":6,"./POW":7,"./PROD":8,"./QUOT":9,"./ROOT":10,"./SUM":11}],13:[function(require,module,exports){
+},{"./DIFF":6,"./POW":7,"./PROD":8,"./QUOT":9,"./SUM":10}],12:[function(require,module,exports){
 var getEscaped = require('./util/getEscaped'),
     delimiters = require('./configure/delimiters'),
     pluck = require('./util/pluck')
@@ -528,7 +514,7 @@ var grammar = function (delimiters) {
 
 module.exports = grammar(delimiters)
 
-},{"./configure/delimiters":5,"./util/getEscaped":15,"./util/pluck":20}],14:[function(require,module,exports){
+},{"./configure/delimiters":5,"./util/getEscaped":14,"./util/pluck":19}],13:[function(require,module,exports){
 function assign (target, source) {
 
     for (key in source) {
@@ -546,7 +532,7 @@ function assign (target, source) {
 
 module.exports = assign
 
-},{}],15:[function(require,module,exports){
+},{}],14:[function(require,module,exports){
 function getEscaped (str) {
     /*
         basically any special character I want to escape, preceded by a \
@@ -559,7 +545,7 @@ function getEscaped (str) {
 
 module.exports = getEscaped
 
-},{}],16:[function(require,module,exports){
+},{}],15:[function(require,module,exports){
 var keys = require('./keys')
 
 function getIndexBy (collection, predicate) {
@@ -582,19 +568,19 @@ function getIndexBy (collection, predicate) {
 }
 
 module.exports = getIndexBy
-},{"./keys":17}],17:[function(require,module,exports){
+},{"./keys":16}],16:[function(require,module,exports){
 function keys (obj) {
     return Object.keys(obj)
 }
 
 module.exports = keys
-},{}],18:[function(require,module,exports){
+},{}],17:[function(require,module,exports){
 function last (collection) {
     return collection[collection.length - 1]
 }
 
 module.exports = last
-},{}],19:[function(require,module,exports){
+},{}],18:[function(require,module,exports){
 function omit (obj, keys) {
 
     var retVal = {}
@@ -614,7 +600,7 @@ function omit (obj, keys) {
 }
 
 module.exports = omit
-},{}],20:[function(require,module,exports){
+},{}],19:[function(require,module,exports){
 function pluck (collection, prop) {
 
     return collection.map(function (item) {
